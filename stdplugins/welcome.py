76603b34acdc01fa86e3df47624e87a5c62b94a8
Ƭƴɭ៩Ʀ Ɖµɾ∂εɳ <dkmsn8@gmail.com>
2020-07-24 10:05:57 +0530

    Update welcome.py

--- stdplugins/welcome.py
+++ stdplugins/welcome.py
@@ -38,7 +38,7 @@ async def _(event):
             update_previous_welcome(event.chat_id, current_message.id)
 
 
-@borg.on(admin_cmd(pattern="savewelcome"))  # pylint:disable=E0602
+@borg.on(admin_cmd("savewelcome"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
